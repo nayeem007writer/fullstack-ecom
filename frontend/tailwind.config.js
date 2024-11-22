6c38ce266ee1bfed4e1d83ee0af1cfdac9cf801b
nayeem-ahammed <nayeemahammed853@gmail.com>
2024-11-22 19:39:52 +0600

    test

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // Include HTML files
+    "./src/**/*.{js,ts,jsx,tsx}",  // Include JavaScript/TypeScript files in your `src` folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
