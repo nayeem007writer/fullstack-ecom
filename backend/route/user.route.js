08c2da400cb773fbe308c2388fe4b899e33bdb76
nayeem-ahammed <nayeemahammed853@gmail.com>
2024-11-22 22:40:48 +0600

    profile GET and PUT api implemented

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, logOutCurrentUser } from "../controller/user.controller.js";
+import { createUser, loginUser, logOutCurrentUser, getCurrentUserProfile, updateCurrentUserProfile } from "../controller/user.controller.js";
 import { authenticate, authorizeAdmin } from "../middleware/auth.middleware.js";
 import { getAllUsers } from "../controller/admin.controller.js";
 const router = express.Router()
@@ -7,5 +7,8 @@ const router = express.Router()
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers)
 router.post('/auth', loginUser)
 router.post('/logout', logOutCurrentUser)
+router.route('/profile')
+    .get(authenticate, getCurrentUserProfile)
+    .put(authenticate, updateCurrentUserProfile)
 
 export default router;
